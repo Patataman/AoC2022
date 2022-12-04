a5a8c3ec29561590fb8fbfa4c947cad2b9bd1f95
patataman <lezdshas@gmail.com>
2022-12-04 10:46:59 +0100

    day 4

---
+++ 004/004.py
@@ -0,0 +1,19 @@
+import sys
+
+with open(sys.argv[1]) as fd:
+    pairs = fd.read().strip().split("\n")
+
+count = 0
+
+for pair in pairs:
+    elf1, elf2 = pair.split(",")
+    elf1 = elf1.split("-")
+    elf1_list = set(range(int(elf1[0]), int(elf1[1])+1))
+    elf2 = elf2.split("-")
+    elf2_list = set(range(int(elf2[0]), int(elf2[1])+1))
+
+    shared_areas = elf1_list | elf2_list
+    if len(shared_areas) == len(elf1_list) or len(shared_areas) == len(elf2_list):
+        count += 1
+
+print(count)
